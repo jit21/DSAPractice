5e98f615c30af1fbf3c3b52b19de42d2bd73dc76
Jit Das <jitdas2012@outlook.com>
2025-07-08 09:17:47 +0530

    add update fuction on segment class

--- Segment Tree/Practice1.java
+++ Segment Tree/Practice1.java
@@ -29,17 +29,42 @@ public class Practice1 {
             return left+right;
 
         }
+        // updating a index in arry so it also effect on our segment tree but we are using O(logn) to update our segment tree
+
+        public void update(int ind,int low,int high,int i,int val){
+            if(low==high) {
+                seg[ind]=val;
+                return;
+            }
+            int mid=(low+high)>>1;
+            if(i> mid){
+                update(2*ind+2,mid+1,high,i,val);
+            }else{
+                update(2*ind+1, low, mid, i, val);
+            }
+            seg[ind]=seg[ind*2+1]+seg[2*ind+2];
+
+        }
     }
     public static void solve(int arr[],int q[][]){
         int n=arr.length;
         ST s1=new ST(n);
         s1.build(0, arr, 0, n-1);
         int m=q.length;
+        System.out.println("Before updating");
         for(int i=0;i<m;i++){
 
             System.out.println("Ans of "+(i+1)+"Query is ->"
             +s1.queries(0, 0, n-1, q[i][0], q[i][1]));
         }
+        System.out.println("After updating ");
+        s1.update(0, 0, n-1, 3, 10);
+         for(int i=0;i<m;i++){
+
+            System.out.println("Ans of "+(i+1)+"Query is ->"
+            +s1.queries(0, 0, n-1, q[i][0], q[i][1]));
+        }
+
 
 
     }
